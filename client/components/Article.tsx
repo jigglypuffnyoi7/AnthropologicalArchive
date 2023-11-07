8a5bb2a1753630c2ab8a52227fea871c9b5b51fe
jordanlim1 <jordanlim2015@gmail.com>
2023-11-07 15:30:30 -0500

    backend functionality complete

--- client/components/Article.tsx
+++ client/components/Article.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
-
+import { useEffect } from 'react';
 
 const Article = () => {
+
+  const getArticle = async () => {
+  const response = await fetch('http://localhost:3000/article/view/Snacks')
+  const data = await response.json()
+  console.log(data)
+  }
+
+  useEffect(() => {
+    getArticle();
+  }, [])
 
 
     return(
--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import Article from './Article';
 import MakePageForm from './MakePageForm';
+import { useState, useEffect } from 'react';
 
 const Home = () => {
+const [titles, setTitles] = useState<string[]>([])
+const [content, setContent] = useState<string[]>([])
 
+  const getHome = async () => {
+    const response = await fetch('http://localhost:3000/article/home')
+    //update state 
+    const data = await response.json()
+    setTitles(data.map((obj:any) => obj.title))
+    // setContent(data.map((obj: any) => obj.content))
+  }
+
+  useEffect(() => {
+    getHome();
+  }, [])
+
+  console.log(titles)
 
     return(
         <>
-          Hi from Home component
+    <h1>HELLO WORLD</h1>
+    <p>THIS IS A HELLO WORLD STATEMENT</p>
+
+
+      {titles}<br></br>
+      {/* {content} */}
+      
           <Article/>
           <MakePageForm/>
         </>
