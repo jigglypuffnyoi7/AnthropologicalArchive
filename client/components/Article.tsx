49d96903190d5503501001d114fcad04f4c4bf62
Katya <katyavillano@gmail.com>
2023-11-07 11:03:13 -0500

    added homepage article page make form page and updated webpack

---
+++ client/components/Article.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Article = () => {
+
+
+    return(
+        <>
+          Hi from article component
+        </>
+    )
+};
+
+export default Article;
---
+++ client/components/MakePageForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const MakePageForm = () => {
+
+
+    return(
+        <>
+          Hi from MakePageForm component
+        </>
+    )
+};
+
+export default MakePageForm;
